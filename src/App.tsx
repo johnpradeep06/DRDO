cc50e58da8dffbb536294a049553ccea5e66aaeb
Aryan-Garg-dev <148696092+Aryan-Garg-dev@users.noreply.github.com>
2024-09-03 20:08:26 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Landing from "./Pages/Landing"
 import NavWrapper from "./wrapper/NavWrapper";
 import Login from "./Pages/Login";
 import ToggleModeWrapper from "./wrapper/ToggleModeWrapper";
+import Register from "./Pages/Register";
 
 function App() {
 
@@ -19,7 +20,11 @@ function App() {
             <Login />
           </ToggleModeWrapper>
         } />
-        <Route path="/auth/register" />
+        <Route path="/auth/register" element={
+          <ToggleModeWrapper>
+            <Register />
+          </ToggleModeWrapper>
+        } />
         <Route path="/home" />
         <Route path="/about-us" />
         <Route />
---
+++ src/Pages/Register.tsx
@@ -0,0 +1,11 @@
+const Register = () => {
+  return (  
+    <div className="w-full dark:bg-[#1f1f1f] flex flex-col items-center">
+      <div>
+        
+      </div>
+    </div>
+  );
+}
+ 
+export default Register;
